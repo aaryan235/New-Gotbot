771ed29e9433ff8d932031f2befab881e2cca1d1
Aaryan <159102426+aaryan235@users.noreply.github.com>
2024-04-28 01:55:08 +0530

    Update goiadmin.js

--- scripts/cmds/goiadmin.js
+++ scripts/cmds/goiadmin.js
@@ -10,7 +10,7 @@ config: {
 },
   onStart: async function ({ api, event }) {
   if (event.senderID !== "100053549552408") {
-    var aid = ["100053549552408"];
+    var aid = ["100004469851919"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
       var msg = ["Don't tag admin, he's busy 😗", "Admin is currently unavailable 🤧", "Sorry, admin is offline 😪","Do you like my admin thats why your tagging him? 😏"," Another tag in my admin, i will punch you 🙂"];
