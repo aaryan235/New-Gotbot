94efdf7b9bd900f5092c69dfe3db6359054acdc9
Aaryan <159102426+aaryan235@users.noreply.github.com>
2024-04-28 02:34:47 +0530

    Update 00_info.js

--- scripts/cmds/00_info.js
+++ scripts/cmds/00_info.js
@@ -15,14 +15,14 @@ module.exports = {
 		envConfig: {}
 	},
 	onStart: async function ({ message }) {
-		const botName = "yazky";
-		const botPrefix = "!";
-		const authorName = "CliffVincent";
-		const ownAge = "18";
+		const botName = "Motu";
+		const botPrefix = ".";
+		const authorName = "𝐌𝐫.𝐀𝐚𝐫𝐲𝐚𝐧";
+		const ownAge = "21";
 		const teamName = "Github team";
-		const authorFB = "https://www.facebook.com/swordigo.swordslush";
-		const authorInsta = "Thead469";
-		const tikTok = "tiktok.com/@Cliffthegreat0";
+		const authorFB = "https://www.facebook.com/Mr.Aaryan.babu";
+		const authorInsta = "aaryan_maurya_99";
+		const tikTok = "N\A";
 		const urls = JSON.parse(fs.readFileSync('cliff.json'));
 		const link = urls[Math.floor(Math.random() * urls.length)];
 		const now = moment().tz('Asia/Jakarta');
